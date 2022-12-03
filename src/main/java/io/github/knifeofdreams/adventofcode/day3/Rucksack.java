996c9fd144bf8e996c8c2dcc23b019df002477be
judito <judito@spotify.com>
2022-12-03 15:30:24 +0000

    Day3 Part2.

--- src/main/java/io/github/knifeofdreams/adventofcode/day3/Rucksack.java
+++ src/main/java/io/github/knifeofdreams/adventofcode/day3/Rucksack.java
@@ -7,42 +7,22 @@ import java.util.List;
 
 public class Rucksack {
 
-  public int sumOfPriorities(List<String> items) {
-    return items.stream()
-        .map(this::findDuplicateItem)
-        .mapToInt(this::calculatePriority)
+  public int sumOfPriorities(List<String> allItems) {
+    return allItems.stream()
+        .mapToInt(
+            items -> {
+              var fullSet = new HashSet<Character>();
+              for (int i = 0; i < items.length() / 2; i++) {
+                fullSet.add(items.charAt(i));
+              }
+              for (int i = items.length() / 2; i < items.length(); i++) {
+                if (fullSet.contains(items.charAt(i))) {
+                  return calculatePriority(items.charAt(i));
+                }
+              }
+              throw new IllegalArgumentException("No duplicate items found between: " + items);
+            })
         .sum();
-  }
-
-  private char findDuplicateItem(String item) {
-    var rightCompartment = new char[item.length() / 2];
-    var leftCompartment = new char[item.length() / 2];
-    item.getChars(0, item.length() / 2, rightCompartment, 0);
-    item.getChars(item.length() / 2, item.length(), leftCompartment, 0);
-    Arrays.sort(rightCompartment);
-    Arrays.sort(leftCompartment);
-
-    int l = 0;
-    int r = 0;
-    while (l < leftCompartment.length || r < rightCompartment.length) {
-      if (leftCompartment[l] == rightCompartment[r]) {
-        return leftCompartment[l];
-      }
-      else if (leftCompartment[l] < rightCompartment[r]) {
-        l += 1;
-      }
-      else {
-        r += 1;
-      }
-    }
-    throw new IllegalArgumentException("No duplicate characters in string");
-  }
-
-  private int calculatePriority(char character) {
-    if (character >= 'a' && character <= 'z') {
-      return character - 'a' + 1;
-    }
-    return character - 'A' + 27;
   }
 
   public int groupBadgePriorities(List<String> items) {
@@ -93,25 +73,34 @@ public class Rucksack {
         .sum();
   }
 
-  public int sumOfPriorities2(List<String> allItems) {
-    return allItems.stream()
-        .mapToInt(
-            items -> {
-              var fullSet = new HashSet<Character>();
-              for (int i = 0; i < items.length() / 2; i++) {
-                fullSet.add(items.charAt(i));
-              }
-              for (int i = items.length() / 2; i < items.length(); i++) {
-                if (fullSet.contains(items.charAt(i))) {
-                  return calculatePriority(items.charAt(i));
-                }
-              }
-              throw new IllegalArgumentException("No duplicate items found between: " + items);
-            })
-        .sum();
+  private char findDuplicateItem(String item) {
+    var rightCompartment = new char[item.length() / 2];
+    var leftCompartment = new char[item.length() / 2];
+    item.getChars(0, item.length() / 2, rightCompartment, 0);
+    item.getChars(item.length() / 2, item.length(), leftCompartment, 0);
+    Arrays.sort(rightCompartment);
+    Arrays.sort(leftCompartment);
+
+    int l = 0;
+    int r = 0;
+    while (l < leftCompartment.length || r < rightCompartment.length) {
+      if (leftCompartment[l] == rightCompartment[r]) {
+        return leftCompartment[l];
+      }
+      else if (leftCompartment[l] < rightCompartment[r]) {
+        l += 1;
+      }
+      else {
+        r += 1;
+      }
+    }
+    throw new IllegalArgumentException("No duplicate characters in string");
   }
 
-  public int groupBadgePriorities2(List<String> items) {
-    return 0;
+  private int calculatePriority(char character) {
+    if (character >= 'a' && character <= 'z') {
+      return character - 'a' + 1;
+    }
+    return character - 'A' + 27;
   }
 }
