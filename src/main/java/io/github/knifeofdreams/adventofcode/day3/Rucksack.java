bb2f2fc54d82b388c8242f0581499c343fe28a2c
judito <judito@spotify.com>
2022-12-03 13:59:28 +0000

    Day3 Part2 in progress

--- src/main/java/io/github/knifeofdreams/adventofcode/day3/Rucksack.java
+++ src/main/java/io/github/knifeofdreams/adventofcode/day3/Rucksack.java
@@ -1,5 +1,6 @@
 package io.github.knifeofdreams.adventofcode.day3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,13 +26,15 @@ public class Rucksack {
     while (l < leftCompartment.length || r < rightCompartment.length) {
       if (leftCompartment[l] == rightCompartment[r]) {
         return leftCompartment[l];
-      } else if (leftCompartment[l] < rightCompartment[r]) {
+      }
+      else if (leftCompartment[l] < rightCompartment[r]) {
         l += 1;
-      } else {
+      }
+      else {
         r += 1;
       }
     }
-    throw new IllegalArgumentException("No duplicate charecters in string");
+    throw new IllegalArgumentException("No duplicate characters in string");
   }
 
   private int calculatePriority(Character character) {
@@ -39,5 +42,53 @@ public class Rucksack {
       return character - 'a' + 1;
     }
     return character - 'A' + 27;
+  }
+
+  public int groupBadgePriorities(List<String> items) {
+    var groups = new ArrayList<List<String>>();
+    var group = new ArrayList<String>();
+    for (String item : items) {
+      if (group.size() < 3) {
+        group.add(item);
+      }
+      else {
+        groups.add(group);
+        group = new ArrayList<>();
+      }
+    }
+
+    return groups.stream()
+        .map(g -> {
+          var first = new char[g.get(0).length()];
+          var second = new char[g.get(1).length()];
+          var third = new char[g.get(2).length()];
+          g.get(0).getChars(0, g.get(0).length(), first, 0);
+          g.get(1).getChars(0, g.get(1).length(), second, 0);
+          g.get(2).getChars(0, g.get(2).length(), third, 0);
+          Arrays.sort(first);
+          Arrays.sort(second);
+          Arrays.sort(third);
+
+          int f = 0;
+          int s = 0;
+          int t = 0;
+          while (f < first.length || s < second.length || t < third.length) {
+            if (first[f] == second[s] && second[s] == third[t]) {
+              return first[f];
+            }
+            else if (first[f] < second[s] && first[f] < third[t]) {
+              f += 1;
+            }
+            else if (second[s] < first[f] && second[s] < third[t]) {
+              s += 1;
+            }
+            else {
+              t += 1;
+            }
+          }
+          throw new IllegalArgumentException("No shared character in group");
+        })
+        .mapToInt(this::calculatePriority)
+        .sum();
   }
 }
--- src/test/java/io/github/knifeofdreams/adventofcode/day3/RucksackTest.java
+++ src/test/java/io/github/knifeofdreams/adventofcode/day3/RucksackTest.java
@@ -5,33 +5,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RucksackTest {
-
-  public static final List<String> EXAMPLE_ITEMS = List.of(
-      "vJrwpWtwJgWrhcsFMMfFFhFp",
-      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-      "PmmdzqPrVvPwwTWBwg",
-      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-      "ttgJtRGJQctTZtZT",
-      "CrZsJsPPZsGzwwsLwLmpwMDw");
+  public static final List<String> EXAMPLE_ITEMS =
+      List.of(
+          "vJrwpWtwJgWrhcsFMMfFFhFp",
+          "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+          "PmmdzqPrVvPwwTWBwg",
+          "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+          "ttgJtRGJQctTZtZT",
+          "CrZsJsPPZsGzwwsLwLmpwMDw");
   public static final List<String> ITEMS = readInput();
   private Rucksack rucksack = new Rucksack();
 
-  @Test
-  public void returnsSumOfPriorities() {
-    assertEquals(157, rucksack.sumOfPriorities(EXAMPLE_ITEMS));
+  @Nested
+  class DuplicateRucksackItemPriorities {
+
+    @Test
+    public void returnsSumOfPriorities() {
+      assertEquals(157, rucksack.sumOfPriorities(EXAMPLE_ITEMS));
+    }
+
+    @Test
+    public void returnsSumOfPrioritiesForMyInput() {
+      assertEquals(8202, rucksack.sumOfPriorities(ITEMS));
+    }
   }
 
-  @Test
-  public void returnsSumOfPrioritiesForMyInput() {
-    assertEquals(8202, rucksack.sumOfPriorities(ITEMS));
+  @Nested
+  class GroupBadges {
+    @Test
+    public void sumsBadgePrioritiesForGroups() {
+      assertEquals(70, rucksack.groupBadgePriorities(ITEMS));
+    }
   }
 
   private static List<String> readInput() {
     try {
-    return Files.readAllLines(Path.of(ClassLoader.getSystemResource("day3/day3-input.txt").toURI()));
+      return Files.readAllLines(
+          Path.of(ClassLoader.getSystemResource("day3/day3-input.txt").toURI()));
     } catch (Exception e) {
       e.printStackTrace();
       throw new IllegalStateException("Cannot read input");
