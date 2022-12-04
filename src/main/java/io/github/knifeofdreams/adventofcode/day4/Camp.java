04035f4fc01d8d3849a3a56a6e6cfce0de2268cf
judito <judito@spotify.com>
2022-12-04 13:50:53 +0000

    Day 4 Part 2.

--- src/main/java/io/github/knifeofdreams/adventofcode/day4/Camp.java
+++ src/main/java/io/github/knifeofdreams/adventofcode/day4/Camp.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Camp {
 
-  public long numberOfOverlaps(List<List<Section>> pairs) {
+  public long numberOfFullOverlaps(List<List<Section>> pairs) {
     return pairs.stream()
         .map(pair -> {
           var section1 = pair.get(0);
@@ -13,6 +13,20 @@ public class Camp {
           return ((section1.beginning <= section2.beginning) && (section1.end >= section2.end))
               || ((section2.beginning <= section1.beginning) && (section2.end >= section1.end));
         })
+        .filter(overlaps -> overlaps)
+        .count();
+  }
+
+  public long numberOfPartialOverlaps(List<List<Section>> pairs) {
+    return pairs.stream()
+        .map(
+            pair -> {
+              var section1 = pair.get(0);
+              var section2 = pair.get(1);
+
+              return (section1.end >= section2.beginning && section1.beginning <= section2.beginning)
+                  || (section2.end >= section1.beginning && section2.beginning <= section1.beginning);
+            })
         .filter(overlaps -> overlaps)
         .count();
   }
--- src/test/java/io/github/knifeofdreams/adventofcode/day4/CampTest.java
+++ src/test/java/io/github/knifeofdreams/adventofcode/day4/CampTest.java
@@ -9,29 +9,46 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class CampTest {
-
-  private static final List<List<Section>> EXAMPLE_PAIRS = List.of(
-      List.of(new Section(2, 4),new Section(6, 8)),
-      List.of(new Section(2, 3), new Section(4, 5)),
-      List.of(new Section(5, 7), new Section(7, 9)),
-      List.of(new Section(2, 8), new Section(3, 7)),
-      List.of(new Section(6, 6), new Section(4, 6)),
-      List.of(new Section(2, 6), new Section(4, 8))
-  );
+  private static final List<List<Section>> EXAMPLE_PAIRS =
+      List.of(
+          List.of(new Section(2, 4), new Section(6, 8)),
+          List.of(new Section(2, 3), new Section(4, 5)),
+          List.of(new Section(5, 7), new Section(7, 9)),
+          List.of(new Section(2, 8), new Section(3, 7)),
+          List.of(new Section(6, 6), new Section(4, 6)),
+          List.of(new Section(2, 6), new Section(4, 8)));
   public static final List<List<Section>> PAIRS = readInput();
   private Camp camp = new Camp();
 
-  @Test
-  public void findFullyContainedPairs() {
-    assertEquals(2, camp.numberOfOverlaps(EXAMPLE_PAIRS));
+  @Nested
+  class FullOverLap {
+
+    @Test
+    public void findFullyContainedPairs() {
+      assertEquals(2, camp.numberOfFullOverlaps(EXAMPLE_PAIRS));
+    }
+
+    @Test
+    public void findFullyContainedPairsForMyInput() {
+      assertEquals(450, camp.numberOfFullOverlaps(PAIRS));
+    }
   }
 
-  @Test
-  public void findFullyContainedPairsForMyInput() {
-    assertEquals(450, camp.numberOfOverlaps(PAIRS));
+  @Nested
+  class PartialOverlap {
+    @Test
+    public void findPartiallyContainedPairs() {
+      assertEquals(4, camp.numberOfPartialOverlaps(EXAMPLE_PAIRS));
+    }
+
+    @Test
+    public void findPartiallyContainedPairsForMyInput() {
+      assertEquals(0, camp.numberOfPartialOverlaps(PAIRS));
+    }
   }
 
   private static List<List<Section>> readInput() {
@@ -39,23 +56,24 @@ class CampTest {
         Files.lines(Path.of(ClassLoader.getSystemResource("day4/day4-input.txt").toURI()))) {
       var pattern = Pattern.compile("(\\d+)-(\\d+),(\\d+)-(\\d+)");
       return input
-          .map(line -> {
-            var matcher = pattern.matcher(line);
-            if (!matcher.matches()) {
-              throw new IllegalArgumentException("Line does not match the pattern: " + line);
-            }
-            var firstPairBeginning = Integer.parseInt(matcher.group(1));
-            var firstPairEnd = Integer.parseInt(matcher.group(2));
-            var secondPairBeginning = Integer.parseInt(matcher.group(3));
-            var secondPairEnd = Integer.parseInt(matcher.group(4));
-            return List.of(new Section(firstPairBeginning, firstPairEnd), new Section(secondPairBeginning, secondPairEnd));
-          })
+          .map(
+              line -> {
+                var matcher = pattern.matcher(line);
+                if (!matcher.matches()) {
+                  throw new IllegalArgumentException("Line does not match the pattern: " + line);
+                }
+                var firstSectionBeginning = Integer.parseInt(matcher.group(1));
+                var firstSectionEnd = Integer.parseInt(matcher.group(2));
+                var secondSectionBeginning = Integer.parseInt(matcher.group(3));
+                var secondSectionEnd = Integer.parseInt(matcher.group(4));
+                return List.of(
+                    new Section(firstSectionBeginning, firstSectionEnd),
+                    new Section(secondSectionBeginning, secondSectionEnd));
+              })
           .collect(Collectors.toList());
     } catch (Exception ex) {
       ex.printStackTrace();
       throw new IllegalStateException("Failed to read file");
     }
   }
-
-
 }
