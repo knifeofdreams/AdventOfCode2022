03740bbd782fec584e6d42001ee81c0707a9dd53
judito <judito@spotify.com>
2022-12-05 19:37:04 +0000

    Day 5 Part 1.

---
+++ src/main/java/io/github/knifeofdreams/adventofcode/day5/SupplyStacks.java
@@ -0,0 +1,30 @@
+package io.github.knifeofdreams.adventofcode.day5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupplyStacks {
+
+  public String rearrange(ArrayList<ArrayList<String>> stacks, List<List<Integer>> moves) {
+    for (List<Integer> move : moves) {
+      var pieces = move.get(0);
+      var from = move.get(1);
+      var to = move.get(2);
+
+      var s = "";
+      for (int i = 0; i < pieces; i++) {
+        s += stacks.get(from - 1).remove(stacks.get(from - 1).size() - 1);
+      }
+
+      for (int i = 0; i < s.length(); i++) {
+        stacks.get(to - 1).add(Character.toString(s.charAt(i)));
+      }
+    }
+
+    var res = "";
+    for (ArrayList<String> box : stacks) {
+      res += box.get(box.size() - 1);
+    }
+    return res;
+  }
+}
