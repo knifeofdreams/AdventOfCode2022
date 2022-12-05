743ee8e6596237805f61d8453bfa255889bd59f0
judito <judito@spotify.com>
2022-12-05 22:03:53 +0000

    Day 5 Part 2 ugly.

--- src/main/java/io/github/knifeofdreams/adventofcode/day5/SupplyStacks.java
+++ src/main/java/io/github/knifeofdreams/adventofcode/day5/SupplyStacks.java
@@ -1,6 +1,7 @@
 package io.github.knifeofdreams.adventofcode.day5;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SupplyStacks {
@@ -18,6 +19,32 @@ public class SupplyStacks {
 
       for (int i = 0; i < s.length(); i++) {
         stacks.get(to - 1).add(Character.toString(s.charAt(i)));
+      }
+    }
+
+    var res = "";
+    for (ArrayList<String> box : stacks) {
+      res += box.get(box.size() - 1);
+    }
+    return res;
+  }
+
+
+  public String rearrangeImproved(ArrayList<ArrayList<String>> stacks, List<List<Integer>> moves) {
+    for (List<Integer> move : moves) {
+      var pieces = move.get(0);
+      var from = move.get(1);
+      var to = move.get(2);
+
+      var tmp = new ArrayList<String>();
+      for (int i = 0; i < pieces; i++) {
+        tmp.add(stacks.get(from - 1).remove(stacks.get(from - 1).size() - 1));
+      }
+
+      Collections.reverse(tmp);
+
+      for (int i = 0; i < tmp.size(); i++) {
+        stacks.get(to - 1).add(tmp.get(i));
       }
     }
 
