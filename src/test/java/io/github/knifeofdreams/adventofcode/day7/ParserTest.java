d34d8c53754ca2015565addc03496ce955855419
judito <judito@spotify.com>
2022-12-08 21:33:38 +0000

    Test renamed accordingly.

--- src/test/java/io/github/knifeofdreams/adventofcode/day7/ParserTest.java
+++ src/test/java/io/github/knifeofdreams/adventofcode/day7/ParserTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
-class FileSystemTest {
+class ParserTest {
 
   @Test
   public void sumOfTotalSizes() {
