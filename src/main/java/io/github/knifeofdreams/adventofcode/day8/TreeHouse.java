a2172bcfdaccad2b1b43f1e7123aa66a70c14b16
judito <judito@spotify.com>
2022-12-09 21:50:34 +0000

    Day 8 Part 1 in progress

--- src/main/java/io/github/knifeofdreams/adventofcode/day8/TreeHouse.java
+++ src/main/java/io/github/knifeofdreams/adventofcode/day8/TreeHouse.java
@@ -1,10 +1,75 @@
 package io.github.knifeofdreams.adventofcode.day8;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TreeHouse {
 
-  public int visibleTrees(List<Object> grid) {
-    return 0;
+  public int visibleTrees(List<List<Integer>> grid) {
+    var visible = 0;
+    var visibleTrees = new ArrayList<>();
+
+    for (List<Integer> row : grid) {
+      var minHeight = -1;
+      for (Integer tree: row) {
+        if (tree > minHeight) {
+          visible += 1;
+          visibleTrees.add(tree);
+          minHeight = tree;
+        }
+      }
+    }
+
+    for (List<Integer> row : grid) {
+      var minHeight = -1;
+      for (int i = row.size() - 1; i > -1; i--) {
+        if (row.get(i) > minHeight) {
+          visible += 1;
+          visibleTrees.add(row.get(i));
+          minHeight = row.get(i);
+        }
+      }
+    }
+
+    var transposedGrid = transpose(grid);
+
+    for (List<Integer> row : transposedGrid) {
+      var minHeight = -1;
+      for (Integer tree: row) {
+        if (tree > minHeight) {
+          visible += 1;
+          visibleTrees.add(tree);
+          minHeight = tree;
+        }
+      }
+    }
+
+    for (List<Integer> row : transposedGrid) {
+      var minHeight = -1;
+      for (int i = row.size() - 1; i > -1; i--) {
+        if (row.get(i) > minHeight) {
+          visible += 1;
+          visibleTrees.add(row.get(i));
+          minHeight = row.get(i);
+        }
+      }
+    }
+
+    return visible;
   }
+
+  private ArrayList<ArrayList<Integer>> transpose(List<List<Integer>> grid) {
+    var transposedGrid = new ArrayList<ArrayList<Integer>>();
+
+    for (int row = 0; row < grid.size(); row++) {
+      var transposedRow = new ArrayList<Integer>();
+      for (int column = 0; column < grid.get(0).size(); column++) {
+        transposedRow.add(grid.get(column).get(row));
+      }
+      transposedGrid.add(transposedRow);
+    }
+
+    return transposedGrid;
+  }
+
 }
--- src/test/java/io/github/knifeofdreams/adventofcode/day8/TreeHouseTest.java
+++ src/test/java/io/github/knifeofdreams/adventofcode/day8/TreeHouseTest.java
@@ -1,17 +1,50 @@
 package io.github.knifeofdreams.adventofcode.day8;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class TreeHouseTest {
 
   @Test
   public void returnsNoOfVisibleTeas() {
-    var grid = List.of();
+    var grid = List.of(
+        List.of(3,0,3,7,3),
+        List.of(2,5,5,1,2),
+        List.of(6,5,3,3,2),
+        List.of(3,3,5,4,9),
+        List.of(3,5,3,9,0)
+    );
     var treeHouse = new TreeHouse();
     assertEquals(21, treeHouse.visibleTrees(grid));
   }
 
+  @Test
+  public void returnsNoOfVisibleTeasForMyInput() {
+    var grid = readInput();
+    var treeHouse = new TreeHouse();
+    assertEquals(0, treeHouse.visibleTrees(grid));
+  }
+
+  private List<List<Integer>> readInput() {
+    try (Stream<String> input =
+        Files.lines(Path.of(ClassLoader.getSystemResource("day8/day8-input.txt").toURI()))) {
+      return input
+          .map(line ->
+            Stream.of(line.split(""))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList())
+          )
+          .collect(Collectors.toList());
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      throw new IllegalStateException("Failed to read file");
+    }
+  }
 }
